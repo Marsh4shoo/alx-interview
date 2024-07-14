70f0a749566f77ef47986c44c77e3b8b0ed393a1
Marsh4shoo <marshal.wjnr@gmail.com>
2024-07-14 16:36:27 +0000

    Updated

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -1,11 +1,16 @@
-in file for testing
+#!/usr/bin/python3
+"""
+Main file for testing
 """
 
 minOperations = __import__('0-minoperations').minOperations
 
 n = 4
-print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
 n = 12
-print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 9
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
